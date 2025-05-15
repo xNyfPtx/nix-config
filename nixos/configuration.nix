e259a8b5a89e52ac114ddc6636cb2de03bcad46e
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com.>
2025-05-15 18:56:26 -0400

    commit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,13 +12,6 @@
       ./ollama.nix
     ];
 
-  boot.loader = {
-   systemd-boot = {
-     enable = true;
-     configurationLimit = 10;
-   };
-   efi.canTouchEfiVariables = true;
-  };
 
 
   nix = {
