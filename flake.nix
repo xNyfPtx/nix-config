6d6314299eeb6226a72056d85a75134edc92b445
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com.>
2025-05-26 14:32:07 -0400

    Update nixpkgs and home-manager to 25.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
