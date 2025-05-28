8898e7117bb41cc11b1ed18fc1ac279915b820af
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-05-27 22:04:50 -0400

    test

--- home/default.nix
+++ home/default.nix
@@ -12,5 +12,24 @@
     stateVersion = "25.05";
   };
 
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Control><Alt>t";
+      command = "wezterm";
+      name = "WezTerm";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = true;
+      night-light-temperature = "uint32 2700";
+    };
+  };
+
   programs.home-manager.enable = true;
 }
