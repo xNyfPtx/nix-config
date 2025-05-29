9fe4476a1c7cfdee6741be6dbd91eb15c4709222
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-05-29 12:50:36 -0400

    Set color-scheme to darkx

--- home/gnome.nix
+++ home/gnome.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 {
   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Control><Alt>t";
       command = "wezterm";
