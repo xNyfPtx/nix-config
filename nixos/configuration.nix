976b039424f01f457788763e27e1233804a7f6e0
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-05-30 21:20:26 -0400

    Rename some options to fix evaluation warning

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -74,7 +74,7 @@
   };
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -91,8 +91,8 @@
 
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "user";
+  services.display.autoLogin.enable = true;
+  services.display.autoLogin.user = "user";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
