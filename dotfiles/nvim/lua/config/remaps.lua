f98e55b28782188a1ab55f134cf73f4d40da37c1
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-06-24 17:08:59 -0400

    Added neovim

---
+++ dotfiles/nvim/lua/config/remaps.lua
@@ -0,0 +1,83 @@
+local map = vim.keymap.set
+
+-- miscellaneous
+map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "move end of line" })
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-l>", "<Right>", { desc = "move right" })
+map("i", "<C-j>", "<Down>", { desc = "move down" })
+map("i", "<C-k>", "<Up>", { desc = "move up" })
+
+map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
+
+-- nvim-telescope/telescope.nvim
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
+map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>")
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
+map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
+map("n", "<leader>ma", "<cmd>Telescope marks<CR>")
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>")
+map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>")
+map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>")
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>")
+map("n", "<leader>pt", "<cmd>Telescope terms<CR>")
+
+-- nvim-tree/nvim-tree.lua
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>")
+
+-- stevearc/conform.nvim
+vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+	conform.format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	})
+end
+)
+
+-- romgrk/barbar.nvim
+
+local opts = { noremap = true, silent = true }
+
+-- romgrk/barbar.nvim | Move to previous/next
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+
+-- romgrk/barbar.nvim | Re-order to previous/next
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+
+-- romgrk/barbar.nvim | Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+
+-- romgrk/barbar.nvim | Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+
+-- romgrk/barbar.nvim | Close buffer
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+
+-- romgrk/barbar.nvim | Magic buffer-picking mode
+map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+map('n', '<C-s-p>', '<Cmd>BufferPickDelete<CR>', opts)
+
+-- romgrk/barbar.nvim | Sort automatically by...
+map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>', opts)
+map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
---
+++ dotfiles/nvim/lua/plugins/conform.lua
@@ -0,0 +1,26 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("conform").setup({
+			--  for users who want auto-save conform + lazyloading!
+			event            = "BufWritePre",
+			format_on_save   = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+			formatters_by_ft = {
+				lua = { "prettier" },
+				nix = { 'nixpkgs_fmt' },
+				["*"] = { "codespell" },
+			},
+		})
+		vim.o.tabstop = 2
+		vim.bo.tabstop = 2
+		vim.o.softtabstop = 2
+		vim.o.shiftround = true
+		vim.o.shiftwidth = 2
+		vim.bo.shiftwidth = 2
+	end,
+}
