7ae8b9c9bb7c389ab0a21352def91cee742cd4e3
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-06-25 17:42:21 -0400

    Format, change LSP for nix and lua, added LSP for html, css, javascript, typescript, json, yaml, markdown

--- dotfiles/nvim/lua/plugins/conform.lua
+++ dotfiles/nvim/lua/plugins/conform.lua
@@ -4,15 +4,22 @@ return {
 	config = function()
 		require("conform").setup({
 			--  for users who want auto-save conform + lazyloading!
-			event            = "BufWritePre",
-			format_on_save   = {
+			event = "BufWritePre",
+			format_on_save = {
 				-- These options will be passed to conform.format()
 				timeout_ms = 500,
 				lsp_fallback = true,
 			},
 			formatters_by_ft = {
-				lua = { "prettier" },
-				nix = { 'nixpkgs_fmt' },
+				html = { "prettierd" },
+				css = { "prettierd" },
+				javascript = { "prettierd" },
+				json = { "prettierd" },
+				yaml = { "prettierd" },
+				markdown = { "prettierd" },
+				typescript = { "prettierd" },
+				lua = { "stylua" },
+				nix = { "alejandra" },
 				["*"] = { "codespell" },
 			},
 		})
