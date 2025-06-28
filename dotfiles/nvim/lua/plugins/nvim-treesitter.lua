38496c9cb77f031b3461cb8e66bcf216f7c04860
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-06-28 18:19:08 -0400

    Make sure html, css, tsx is install in treesitter

--- dotfiles/nvim/lua/plugins/nvim-treesitter.lua
+++ dotfiles/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,4 +3,15 @@ return {
 	branch = "master",
 	lazy = false,
 	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"html",
+				"css",
+				"tsx",
+			},
+			sync_install = false,
+			auto_install = true,
+		})
+	end,
 }
