dd0a14c2e2c261cde32332cf5b5e69d56d9665ac
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-07-11 16:53:00 -0400

    Removed plugin update notification

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -28,5 +28,5 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "catppuccin-mocha" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = { enabled = true, notify = false },
 })
