a90622c8d51229f587d4946a0187366afaa7bb33
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-07-11 15:45:10 -0400

    Added boot.nix

---
+++ nixos/boot.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
+    efi.canTouchEfiVariables = true;
+  };
+}
