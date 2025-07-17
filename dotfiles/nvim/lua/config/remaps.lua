f42b4aa8d16020918729c68cc6af1b1e2360fddc
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-07-17 13:13:31 -0400

    Added lazygit.nvim

--- dotfiles/nvim/lua/config/remaps.lua
+++ dotfiles/nvim/lua/config/remaps.lua
@@ -16,25 +16,28 @@ map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
 map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
 map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
 
-map("n", "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
+map("n", "<C-s>", "<cmd>w<cr>", { desc = "general save file" })
 
 map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 
 -- nvim-telescope/telescope.nvim
-map("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
-map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>")
-map("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
-map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
-map("n", "<leader>ma", "<cmd>Telescope marks<CR>")
-map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>")
-map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>")
-map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>")
-map("n", "<leader>gt", "<cmd>Telescope git_status<CR>")
-map("n", "<leader>pt", "<cmd>Telescope terms<CR>")
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+map("n", "<leader>fw", "<cmd>Telescope live_grep<cr>")
+map("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+map("n", "<leader>ma", "<cmd>Telescope marks<cr>")
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>")
+map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<cr>")
+map("n", "<leader>cm", "<cmd>Telescope git_commits<cr>")
+map("n", "<leader>gt", "<cmd>Telescope git_status<cr>")
+map("n", "<leader>pt", "<cmd>Telescope terms<cr>")
 
 -- nvim-tree/nvim-tree.lua
-map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
-map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>")
+map("n", "<C-n>", "<cmd>NvimTreeToggle<cr>")
+map("n", "<leader>e", "<cmd>NvimTreeFocus<cr>")
+
+-- kdheepak/lazygit.nvim
+map("n", "<leader>lg", "<cmd>LazyGit<cr>")
 
 -- stevearc/conform.nvim
 vim.keymap.set({ "n", "v" }, "<leader>mp", function()
@@ -46,45 +49,45 @@ vim.keymap.set({ "n", "v" }, "<leader>mp", function()
 end)
 
 -- cjodo/convert.nvim
-map("n", "<leader>cn", "<cmd>ConvertFindNext<CR>")
-map("n", "<leader>cc", "<cmd>ConvertFindCurrent<CR>")
-map({ "n", "v" }, "<leader>ca", "<cmd>ConvertAll<CR>")
+map("n", "<leader>cn", "<cmd>ConvertFindNext<cr>")
+map("n", "<leader>cc", "<cmd>ConvertFindCurrent<cr>")
+map({ "n", "v" }, "<leader>ca", "<cmd>ConvertAll<cr>")
 
 -- romgrk/barbar.nvim
 
 -- romgrk/barbar.nvim | Move to previous/next
-map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
-map("n", "<A-.>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
+map("n", "<A-,>", "<cmd>BufferPrevious<cr>", { noremap = true, silent = true })
+map("n", "<A-.>", "<cmd>BufferNext<cr>", { noremap = true, silent = true })
 
 -- romgrk/barbar.nvim | Re-order to previous/next
-map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", { noremap = true, silent = true })
-map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", { noremap = true, silent = true })
+map("n", "<A-<>", "<cmd>BufferMovePrevious<cr>", { noremap = true, silent = true })
+map("n", "<A->>", "<cmd>BufferMoveNext<cr>", { noremap = true, silent = true })
 
 -- romgrk/barbar.nvim | Goto buffer in position...
-map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", { noremap = true, silent = true })
-map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", { noremap = true, silent = true })
-map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", { noremap = true, silent = true })
-map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", { noremap = true, silent = true })
-map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", { noremap = true, silent = true })
-map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", { noremap = true, silent = true })
-map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", { noremap = true, silent = true })
-map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", { noremap = true, silent = true })
-map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", { noremap = true, silent = true })
-map("n", "<A-0>", "<Cmd>BufferLast<CR>", { noremap = true, silent = true })
+map("n", "<A-1>", "<cmd>BufferGoto 1<cr>", { noremap = true, silent = true })
+map("n", "<A-2>", "<cmd>BufferGoto 2<cr>", { noremap = true, silent = true })
+map("n", "<A-3>", "<cmd>BufferGoto 3<cr>", { noremap = true, silent = true })
+map("n", "<A-4>", "<cmd>BufferGoto 4<cr>", { noremap = true, silent = true })
+map("n", "<A-5>", "<cmd>BufferGoto 5<cr>", { noremap = true, silent = true })
+map("n", "<A-6>", "<cmd>BufferGoto 6<cr>", { noremap = true, silent = true })
+map("n", "<A-7>", "<cmd>BufferGoto 7<cr>", { noremap = true, silent = true })
+map("n", "<A-8>", "<cmd>BufferGoto 8<cr>", { noremap = true, silent = true })
+map("n", "<A-9>", "<cmd>BufferGoto 9<cr>", { noremap = true, silent = true })
+map("n", "<A-0>", "<cmd>BufferLast<cr>", { noremap = true, silent = true })
 
 -- romgrk/barbar.nvim | Pin/unpin buffer
-map("n", "<A-p>", "<Cmd>BufferPin<CR>", { noremap = true, silent = true })
+map("n", "<A-p>", "<cmd>BufferPin<cr>", { noremap = true, silent = true })
 
 -- romgrk/barbar.nvim | Close buffer
-map("n", "<A-c>", "<Cmd>BufferClose<CR>", { noremap = true, silent = true })
+map("n", "<A-c>", "<cmd>BufferClose<cr>", { noremap = true, silent = true })
 
 -- romgrk/barbar.nvim | Magic buffer-picking mode
-map("n", "<C-p>", "<Cmd>BufferPick<CR>", { noremap = true, silent = true })
-map("n", "<C-s-p>", "<Cmd>BufferPickDelete<CR>", { noremap = true, silent = true })
+map("n", "<C-p>", "<cmd>BufferPick<cr>", { noremap = true, silent = true })
+map("n", "<C-s-p>", "<cmd>BufferPickDelete<cr>", { noremap = true, silent = true })
 
 -- romgrk/barbar.nvim | Sort automatically by...
-map("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", { noremap = true, silent = true })
-map("n", "<Space>bn", "<Cmd>BufferOrderByName<CR>", { noremap = true, silent = true })
-map("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", { noremap = true, silent = true })
-map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", { noremap = true, silent = true })
-map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", { noremap = true, silent = true })
+map("n", "<Space>bb", "<cmd>BufferOrderByBufferNumber<cr>", { noremap = true, silent = true })
+map("n", "<Space>bn", "<cmd>BufferOrderByName<cr>", { noremap = true, silent = true })
+map("n", "<Space>bd", "<cmd>BufferOrderByDirectory<cr>", { noremap = true, silent = true })
+map("n", "<Space>bl", "<cmd>BufferOrderByLanguage<cr>", { noremap = true, silent = true })
+map("n", "<Space>bw", "<cmd>BufferOrderByWindowNumber<cr>", { noremap = true, silent = true })
