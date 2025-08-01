99b3977d8bb7351104a278770b0fff44b12428f3
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-08-01 12:14:42 -0400

    Moved options to options.lua

---
+++ dotfiles/nvim/lua/config/options.lua
@@ -0,0 +1,19 @@
+local g = vim.g
+local opt = vim.opt
+
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+
+opt.cursorline = true
+opt.breakindent = true
+opt.ignorecase = true
+opt.number = true
+opt.splitbelow = true
+opt.scrolloff = 10
+opt.shiftround = true
+opt.shiftwidth = 2
+opt.showmode = false
+opt.smartcase = true
+opt.softtabstop = 2
+opt.splitright = true
+opt.tabstop = 2
